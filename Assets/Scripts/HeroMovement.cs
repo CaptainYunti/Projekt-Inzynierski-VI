8f2a1d275834ec6a3bdd077d04b84493c4b47ead
Yunti <n.pawcio@gmail.com>
2019-11-13 21:39:03 +0100

    Skok
    Pojedynczy skok dla bohatera

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -6,18 +6,29 @@ public class HeroMovement : MonoBehaviour
 {
 
     public float speed;
+    bool isJumpReady;
+
+    [SerializeField]
+    float jumpHigh;
+
+    Collision ground;
 
     Vector3 direction;
+    float maxJumpValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxJumpValue = float.MinValue;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        isJumpReady = ReadyToJump(ground);
+        //isJumpReady = false;
+
         if (Input.anyKey)
         {
             Move();
@@ -28,19 +39,59 @@ public class HeroMovement : MonoBehaviour
     private void Move()
     {
         direction = Vector3.zero;
+        Vector3 jumpDirection = Vector3.zero;
+
+
 
         if (Input.GetKey("w"))
-            direction = Vector3.forward;
+            direction += Vector3.forward;
         if (Input.GetKey("a"))
-            direction = Vector3.left;
+            direction += Vector3.left;
         if (Input.GetKey("s"))
-            direction = Vector3.back;
+            direction += Vector3.back;
         if (Input.GetKey("d"))
-            direction = Vector3.right;
-        if (Input.GetKey("space"))
-            direction = Vector3.up;
+            direction += Vector3.right;
+        if (Input.GetKey("space") && isJumpReady)
+        {
+            jumpDirection = Vector3.up;
+        }
+
 
         transform.localPosition += direction * speed * Time.deltaTime;
+        transform.localPosition += jumpDirection * jumpHigh * Time.deltaTime;
+
+    }
+
+
+    private bool ReadyToJump(Collision ground)
+    {
+
+        //if zamienip=ony na OnCollisionStay
+       /* if (ground.gameObject.tag == "ground")
+        {
+            maxJumpValue = float.MinValue;
+            return true;
+        }*/
+           // OnCollisionStay(ground);
+
+            if (maxJumpValue < transform.position.y)
+                maxJumpValue = transform.position.y;
+            else
+                return false;
+
+
+        return true;
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject)
+        {
+            maxJumpValue = float.MinValue;
+            isJumpReady = true;
+        }
+        
     }
 
 }
