8be00819d898527eaa449464cf979e60815eea53
Yunti <n.pawcio@gmail.com>
2019-11-14 02:22:34 +0100

    Skrypt statystyk - początek
    Utworzenie skryptu, w którym będą statystyki

---
+++ Assets/Scripts/HeroStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroStats : MonoBehaviour
+{
+    [SerializeField]
+    int strength;
+    [SerializeField]
+    int level;
+    [SerializeField]
+    int health;
+
+
+
+    int exp;
+    int expNextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
