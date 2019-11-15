48d45a750edba468cf7aa31825f462b902a1792b
Yunti <n.pawcio@gmail.com>
2019-11-15 20:11:00 +0100

    Zmiana kamery i collider dzioba
    Dodanie zmiany kamery na kamerę z góry i z przodu. Dodano collider dzioba.

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -24,12 +24,18 @@ public class HeroMovement : MonoBehaviour
     float maxJumpValue;
     float yaw, pitch;
 
+    Camera backCamera, frontCamera, birdViewCamera,
+        izzyTalkCamera, izzySideCamera;
+    Camera[] cameras;
+
     // Start is called before the first frame update
     void Start()
     {
         maxJumpValue = float.MinValue;
         cameraRotation = new Vector3(0, 0, 0);
         yaw = pitch = 0;
+        SetCameras();
+
     }
 
     // Update is called once per frame
@@ -39,15 +45,22 @@ public class HeroMovement : MonoBehaviour
         isJumpReady = ReadyToJump(ground);
         //isJumpReady = false;
 
+        if(Input.GetKey("k"))
+        {
+
+        }
+        else
+
         if (Input.anyKey)
         {
             Move();
         }
+
     }
 
     private void LateUpdate()
     {
-        Camera.current.transform.localEulerAngles = cameraRotation * mouseSens;
+        Camera.main.transform.localEulerAngles = cameraRotation * mouseSens;
     }
 
 
@@ -84,6 +97,38 @@ public class HeroMovement : MonoBehaviour
             //yaw = pitch = 0;
             cameraRotation = new Vector3(0, 0, 0);
         }
+
+        if(Input.GetKeyDown("1"))
+        {
+            foreach(Camera c in cameras)
+            {
+                if (c == backCamera)
+                    c.enabled = true;
+                else
+                    c.enabled = false;
+            }
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            foreach (Camera c in cameras)
+            {
+                if (c == frontCamera)
+                    c.enabled = true;
+                else
+                    c.enabled = false;
+            }
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            foreach (Camera c in cameras)
+            {
+                if (c == birdViewCamera)
+                    c.enabled = true;
+                else
+                    c.enabled = false;
+            }
+        }
+
 
         //rotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
 
@@ -126,6 +171,29 @@ public class HeroMovement : MonoBehaviour
         }
         
     }
+
+    private void SetCameras()
+    {
+
+        cameras = Camera.allCameras;
+
+        backCamera = Camera.main;
+
+        foreach(Camera c in cameras)
+        {
+            if (c.name == "Camera Bird View")
+                birdViewCamera = c;
+            if (c.name == "Camera Izzy Side")
+                izzySideCamera = c;
+            if (c.name == "Camera Izzy")
+                izzyTalkCamera = c;
+            if (c.name == "Camera Chicken Front")
+                frontCamera = c;
+        }
+
+    }
+
+
 
 
 
