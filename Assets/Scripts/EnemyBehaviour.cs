14b3872fe35e8251f5f4100040b953fdda695955
Yunti <n.pawcio@gmail.com>
2019-11-16 23:02:42 +0100

    Wirtualny wróg
    Stworzenie abstrakcyjnej klasy dla przeciwników

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBehaviour : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    //public int strength;
+    //public float attackSpeed;
+    public float range;
+
+    public abstract void AttackHero();
+    public abstract void AttackNPC();
+    public abstract void DefendNPC();
+    public bool IsInRange(GameObject player)
+    {
+        return false;
+        //tu bedzie cos mondrego xD
+    }
+    public abstract void Run();
+    public abstract void RunRandom();
+    public abstract void NormalBehaviour();
+    public abstract void Move();
+    public bool IsDead()
+    {
+        if (currentHP > 0)
+            return false;
+        return true;
+    }
+    public void GetDamage(int damage)
+    {
+        currentHP -= damage;
+        if(IsDead())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
