f8c4ac432ffe32d2a8f15c0995b5b7f82a0f8e30
Yunti <n.pawcio@gmail.com>
2019-11-16 21:49:50 +0100

    Animacja ataku
    Kurczak-bohater dostał animację ataku.

---
+++ Assets/Scripts/HeroAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAttack : MonoBehaviour
+{
+
+    Animator anim;
+    bool eat;
+    GameObject beak;
+    SphereCollider beakCollider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        eat = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            eat = true;
+        }
+        else
+        {
+            eat = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetBool("Eat", eat);
+    }
+}
